b78a9d859c840a4df8c867a354ee60eaae004c4b
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-08-22 14:46:39 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/d/WwwApplication.java
@@ -0,0 +1,12 @@
+package d;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(WwwApplication.class, args);
+	}
+
+}
